8f0299a1fc385177b6ff0f7f42d1ccb682c67911
FedericoCanale <icocanale@gmail.com>
2025-11-18 12:44:44 +0100

    Wrap App with BudgetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 
+import { BudgetProvider } from "./contexts/BudgetContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </React.StrictMode>
 );
