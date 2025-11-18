cb6f17db45e859ecd3ad62b60e0077a274dcaa49
FedericoCanale <icocanale@gmail.com>
2025-11-18 17:02:55 +0100

    Add ilter products by budgetMode in ProductsPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,22 @@
-import products from "../data/products";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import products from "../data/products";
+import BudgetContext from "../contexts/BudgetContext";
 
 export default function ProductsPage() {
+    const { budgetMode } = useContext(BudgetContext);
+
+    // Se budgetMode === true allora mostro solo prodotti con prezzo <= 30
+    const filteredProducts = budgetMode
+        ? products.filter((p) => p.price <= 30)
+        : products;
+
     return (
         <div className="products-page">
             <h1 className="products-title">Prodotti</h1>
 
             <div className="products-grid">
-                {products.map((product) => (
+                {filteredProducts.map((product) => (
                     <Link
                         to={`/prodotti/${product.id}`}
                         key={product.id}
@@ -26,9 +35,11 @@ export default function ProductsPage() {
                             <p className="product-price">
                                 <strong>Prezzo:</strong> {product.price} €
                             </p>
+
                             <p className="product-category">
                                 <strong>Categoria:</strong> {product.category}
                             </p>
+
                             <p className="product-description">
                                 {product.description}
                             </p>
