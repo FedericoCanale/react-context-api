6e47ce0b23ac9ddf0af3aef9fa4a466498383c05
FedericoCanale <icocanale@gmail.com>
2025-11-18 12:35:48 +0100

    Add BudgetProvider with budgetMode state

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -1,5 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const BudgetContext = createContext();
+
+export function BudgetProvider({ children }) {
+    const [budgetMode, setBudgetMode] = useState(false);
+
+    const value = {
+        budgetMode,
+        setBudgetMode,
+    };
+
+    return (
+        <BudgetContext.Provider value={value}>
+            {children}
+        </BudgetContext.Provider>
+    );
+}
 
 export default BudgetContext;
